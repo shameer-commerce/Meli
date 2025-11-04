7b0f6030180331db5d1478eef75d5f99f9ae215f
shameer-commerce <shameer.commerce@gmail.com>
2025-11-04 18:24:57 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function goToParts() {
+  window.location.href = "parts.html";
+}
+
+function goBack() {
+  window.location.href = "parts.html";
+}
+
+// When on reasons page, load the part info
+document.addEventListener("DOMContentLoaded", () => {
+  // This function is no longer needed as we have individual reason pages
+});
